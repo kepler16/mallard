1534926a05a737a8d38ad07960e97d2912a6d68e
Julien Vincent <m@julienvincent.io>
2024-03-21 11:33:39 +0200

    patch: Allow passing operations directly to run!

--- packages/mallard/src/k16/mallard/dev.clj
+++ packages/mallard/src/k16/mallard/dev.clj
@@ -1,6 +1,7 @@
 (ns k16.mallard.dev
   "For use in development, this is a component that can be included in a gx configuration to automatically
   run any migrations on system start."
+  (:refer-clojure :exclude [run!])
   (:require
    [k16.mallard.api :as api]
    [k16.mallard.datastore :as datastore.api]
@@ -20,6 +21,7 @@
       [:or executor/?Operations
        [:=> [:cat] executor/?Operations]]]]]])
 
+#_{:clj-kondo/ignore [:clojure-lsp/unused-public-var]}
 (def run!
   {:gx/start
    {:gx/processor (fn [{:keys [props]}]
